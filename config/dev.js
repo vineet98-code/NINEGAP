e2c3c9c1c902dfc17d059e3b38437df3717dfaa5
Vineet <vineet98love@gmail.com>
2022-10-22 11:28:42 +0530

    update

--- config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MOGOURI: 'mongodb+srv://ninegap:Q2xsTmEVgaQ4zyVc@cluster0.9eudg2r.mongodb.net/?retryWrites=true&w=majority'
-}
-
-
--- config/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    
-    MOGOURI:process.env.MOGOURI
-}
